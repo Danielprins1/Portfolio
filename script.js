8dcbe053753df6a9eac4c37e95c209ad0174623c
Danielprins1 <145456478+Danielprins1@users.noreply.github.com>
2024-05-14 00:04:16 +0200

    grote verandering

--- script.js
+++ script.js
@@ -47,3 +47,12 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("prevBtn").addEventListener("click", slidePrev);
 });
 
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 3){
+        counter = 1;
+     }
+    }, 5000);
